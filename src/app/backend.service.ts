40d323b11221022e09cd80c254c8dffddc2a766c
Thomas Lind <thompa@vpnclient53.remote-access.kth.se>
2018-12-01 01:00:54 +0100

    commit

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -5,7 +5,7 @@ import { map, catchError, tap } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json'
+    //'Content-Type':  'application/json'
   })
 };
 
