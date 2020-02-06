b6ee32a29b5154be5c2e3e388090656cee8369f5
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-02-06 11:31:31 +0100

    ref miljö

--- src/environments/environment.ref.ts
+++ src/environments/environment.ref.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  server: "https://kthb-hv.lib.kth.se",
+  server: "https://apps-ref.lib.kth.se",
   configurl: "/kthbforms/assets/appConfig.json",
   //lägger på formulärets id + .json när den hämtas i dynamic-form
   formdataurl: "/kthbforms/assets/"
